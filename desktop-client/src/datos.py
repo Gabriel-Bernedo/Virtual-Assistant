59c7f3e01b3474f703a03e35c7bbee820ebfe644
Paulo Hidalgo <phidalgo@unsa.edu.pe>
2023-10-20 10:56:25 -0500

    migraciones de bases de datos

--- desktop-client/src/datos.py
+++ desktop-client/src/datos.py
@@ -2,5 +2,5 @@ import json
 with open('basedatos.json', 'r') as archivo:
     datos = json.load(archivo)
 
-with open('basedatospreguntas.json', 'r') as archivo:
+with open('preguntas.json', 'r') as archivo:
     preguntas = json.load(archivo)
