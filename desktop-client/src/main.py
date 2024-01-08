de53f140ab769b83882015b86930a6b934d947af
Taylor Betanzos <tbetanzos@unsa.edu.pe>
2024-01-07 22:56:16 -0500

    ahorcado al 90%

--- desktop-client/src/main.py
+++ desktop-client/src/main.py
@@ -1,14 +1,19 @@
+import time
+
 from auxiliar import *
 import threading
-# from juegos.quizApp import *
+from juegos import *
 from utils_audio import *
 import sys
-
 def programa():
     decir(datos['bienvenida'][0])
-    print("Di tu nombre: ")
     nombre = escuchar().capitalize()
     decir(f"Hola {nombre}. Mucho gusto.")
+    #decir("Hola.")
+    #estado['termino'] = True
+    #time.sleep(1)
+    #ahorcado()
+    #return
     decir(f"{nombre} Ahora voy a explicarte sobre las opciones que tiene este programa. Tienes 3 opciones para escoger.")
     decir("La opción Aprendizaje es donde podrás aprender todo con respecto a la Estructura de un computador. "
                "La opción Pruebas es donde podrás poner en práctica lo que aprendiste mediante exámenes. "
@@ -17,6 +22,7 @@ def programa():
         decir("¿Qué opción eliges? ")
         decir(" 1) Aprendizaje\n 2) Pruebas\n 3) Juegos\n 4) Salir")
         respuesta = escuchar()
+        #respuesta = 'juegos'
         if respuesta == "aprendizaje":
             decir("Elegiste la opcion APRENDIZAJE.")
             while True:
@@ -109,82 +115,80 @@ def programa():
                 else:
                     decir("repite la opcion por favor")
         elif respuesta == "juegos":
-            decir("Elegiste la opción JUEGOS.")
-            decir(
-                "El primer juego consta en contestar las preguntas, haciendo click en la imagen que crees que es la respuesta.")
-            if __name__ == "__main__":
-                '''root = tk.Tk()
-                app = ComputerStructureQuizApp(root)
-                root.mainloop()'''
-                print('ingresaste a juegos')
+            decir("Ahora jugaras ahorcado")
+            estado['asistente'] = False
+            while not estado['asistente']:
+                time.sleep(1)
         elif respuesta == "salir":
             break
         else:
-            decir(
-                nombre + " creo que no has respondido con alguna de las instrucciones indicadas anteriormente")
+            #decir(
+            #    nombre + " creo que no has respondido con alguna de las instrucciones indicadas anteriormente")
             decir("Responde con una de las alternativas mencionadas.")
+    estado['termino'] = True
 
-pygame.init()
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-CELESTE = (85, 201, 245)
-
-clock = pygame.time.Clock()
-fondo = pygame.image.load('img/fondo.jpg')#.convert()#600x600px
-
-size = (fondo.get_width(),fondo.get_height())  # ancho,alto
-pygame.display.set_icon(fondo)
-pygame.display.set_caption('PYG-4')
-screen = pygame.display.set_mode(size)
-#screen.fill(WHITE)
-
-micro = pygame.image.load('img/micro.png').convert_alpha()#120x120px
-load = [pygame.image.load('img/load.png').convert_alpha()]#140x140px
-
-fuente = pygame.font.SysFont('segoe print', 20)
-pygTxt = fuente.render('PYG-4 Tu Asistente Virtual', True, WHITE)
-pygame.display.flip()
 
 def asistentePyg():#INTERFAZ grafica
-    modo = True
     hilo1 = threading.Thread(target=programa)
     hilo1.start()
+    interfaz()
+def interfaz():
+    pygame.init()
+    fondo = pygame.image.load('img/fondo.jpg')  # .convert()#600x600px
+
+    size = (fondo.get_width(), fondo.get_height())  # ancho,alto
+    pygame.display.set_mode(size)
+
+    micro = pygame.image.load('img/micro.png').convert_alpha()  # 120x120px
+    load = [pygame.image.load('img/load.png').convert_alpha()]  # 140x140px
+
+    fuente = pygame.font.SysFont('segoe print', 20)
+    pygTxt = fuente.render('PYG-4 Tu Asistente Virtual', True, WHITE)
+    pygame.display.flip()
+    modo = True
     while not estado['termino']:
-        subtitulos = fuente.render(subTxt[0], True, WHITE)
-        # print('iterando')
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                estado['termino'] = True
-                sys.exit()
-        screen.blit(fondo, (0, 0))
-        if estado['hablando']:
-            ancho = 70
-            if modo:#185-120 = x,y 405 -> 220 -> 210
-                alto = 150
-                pygame.draw.ellipse(screen, RED, (185, 140, ancho, alto-40))
-                pygame.draw.ellipse(screen, BLUE, (260, 120, ancho, alto))
-                pygame.draw.ellipse(screen, GREEN, (335, 140, ancho, alto-40))
+        screen = pygame.display.set_mode(size)
+
+        while estado['asistente']:
+            pygame.display.set_icon(fondo)
+            pygame.display.set_caption('PYG-4')
+            subtitulos = fuente.render(subTxt[0], True, WHITE)
+            # print('iterando')
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    estado['termino'] = True
+                    sys.exit()
+            screen.blit(fondo, (0, 0))
+            if estado['hablando']:
+                ancho = 70
+                if modo:#185-120 = x,y 405 -> 220 -> 210
+                    alto = 150
+                    pygame.draw.ellipse(screen, RED, (185, 140, ancho, alto-40))
+                    pygame.draw.ellipse(screen, BLUE, (260, 120, ancho, alto))
+                    pygame.draw.ellipse(screen, GREEN, (335, 140, ancho, alto-40))
+                else:
+                    alto = 90
+                    pygame.draw.ellipse(screen, RED, (185, 140, ancho, alto+40))
+                    pygame.draw.ellipse(screen, BLUE, (260, 160, ancho, alto))
+                    pygame.draw.ellipse(screen, GREEN, (335, 140, ancho, alto+40))
+                clock.tick(2.5)
+                modo = not modo
+            elif estado['escuchando']:#120+150=270/2=135
+                if modo:  # 185-120 = x,y 405 -> 220 -> 2109
+                    pygame.draw.circle(screen, CELESTE, (295, 230),80)
+                modo = not modo
+                screen.blit(micro, (235, 170))
+                clock.tick(2)
             else:
-                alto = 90
-                pygame.draw.ellipse(screen, RED, (185, 140, ancho, alto+40))
-                pygame.draw.ellipse(screen, BLUE, (260, 160, ancho, alto))
-                pygame.draw.ellipse(screen, GREEN, (335, 140, ancho, alto+40))
-            clock.tick(2.5)
-            modo = not modo
-        elif estado['escuchando']:#120+150=270/2=135
-            if modo:  # 185-120 = x,y 405 -> 220 -> 2109
-                pygame.draw.circle(screen, CELESTE, (295, 230),80)
-            modo = not modo
-            screen.blit(micro, (235, 170))
-            clock.tick(2)
-        else:
-            screen.blit(load[0], (235, 170))
-            load[0] = pygame.transform.rotate(load[0], 90)
-            clock.tick(1.5)
-        screen.blit(pygTxt, (10, 10))
-        screen.blit(subtitulos, (185, 160))
-        pygame.display.flip()
+                screen.blit(load[0], (235, 170))
+                load[0] = pygame.transform.rotate(load[0], 90)
+                clock.tick(1.5)
+            screen.blit(pygTxt, (10, 10))
+            screen.blit(subtitulos, (185, 160))
+            pygame.display.flip()
+        while not estado['asistente']:
+            ahorcado()
+            estado['jugando'] = True
+            estado['asistente'] = True
+
 asistentePyg()
